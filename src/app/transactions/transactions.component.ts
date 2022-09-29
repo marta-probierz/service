2786cc8749788bc615f69ec4de2d2ae9e4d988c7
Marta Probierz <m.probierz@wearevirtua.com>
2022-09-29 10:57:34 +0200

    VA-18 add Table sorting and filtering

--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { SortEvent } from 'primeng/api';
 
 import { Transaction } from '@app/transactions/Transaction';
 import { TransactionsService } from '@app/services/transactions.service';
@@ -30,6 +31,21 @@ export class TransactionsComponent implements OnInit {
             { field: 'pendingAmountDue', header: 'Pending Amount DUE', icon: false },
             { field: 'lastActivityDate', header: 'Last Activity Date', icon: false }
         ];
+    }
+
+    customSort(event: SortEvent) {
+        event.data.sort((data1, data2) => {
+            const value1 = data1[event.field];
+            const value2 = data2[event.field];
+            let result = null;
+            if (value1 == null && value2 != null) { result = -1; }
+            else if (value1 != null && value2 == null) { result = 1; }
+            else if (value1 == null && value2 == null) { result = 0; }
+            else if (typeof value1 === 'string' && typeof value2 === 'string') { result = value1.localeCompare(value2); }
+            else { result = (value1 < value2) ? -1 : (value1 > value2) ? 1 : 0; }
+
+            return (event.order * result);
+        });
     }
 }
 
